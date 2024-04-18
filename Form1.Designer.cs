f5ad7e874a90bb6e03b9c615b01a9cf77a4e4b86
Sotirios <sotirios.lukeris644@edu1.pgzaimov.com>
2024-04-18 09:52:09 +0300

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace TreesZaimov
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox4 = new ComboBox();
+            comboBox5 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox6 = new ComboBox();
+            label7 = new Label();
+            button1 = new Button();
+            btn = new Button();
+            textBox2 = new TextBox();
+            label8 = new Label();
+            textBox3 = new TextBox();
+            label9 = new Label();
+            pictureBox1 = new PictureBox();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 58);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Наименование";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(40, 116);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(158, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(40, 175);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(158, 23);
+            comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(40, 231);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(158, 23);
+            comboBox3.TabIndex = 4;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(40, 289);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(158, 23);
+            comboBox4.TabIndex = 5;
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(40, 346);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(158, 23);
+            comboBox5.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(40, 98);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 7;
+            label2.Text = "OTDEL";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 157);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 15);
+            label3.TabIndex = 8;
+            label3.Text = "CLASS";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(40, 213);
+            label4.Name = "label4";
+            label4.Size = new Size(42, 15);
+            label4.TabIndex = 9;
+            label4.Text = "Razred";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(40, 271);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Rod";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(40, 328);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 15);
+            label6.TabIndex = 11;
+            label6.Text = "latitude";
+            // 
+            // comboBox6
+            // 
+            comboBox6.FormattingEnabled = true;
+            comboBox6.Location = new Point(40, 405);
+            comboBox6.Name = "comboBox6";
+            comboBox6.Size = new Size(158, 23);
+            comboBox6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(40, 387);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 15);
+            label7.TabIndex = 13;
+            label7.Text = "longtitude";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(297, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 58);
+            button1.TabIndex = 14;
+            button1.Text = "INSERT TREE";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // btn
+            // 
+            btn.Location = new Point(297, 228);
+            btn.Name = "btn";
+            btn.Size = new Size(145, 58);
+            btn.TabIndex = 15;
+            btn.Text = "button2";
+            btn.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(463, 405);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(267, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(463, 371);
+            label8.Name = "label8";
+            label8.Size = new Size(38, 15);
+            label8.TabIndex = 17;
+            label8.Text = "label8";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(333, 58);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(74, 23);
+            textBox3.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(333, 30);
+            label9.Name = "label9";
+            label9.Size = new Size(106, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Потребителско ID";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.InitialImage = Trees.Properties.Resources.spinning;
+            pictureBox1.Location = new Point(813, 123);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(241, 220);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 21;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(479, 37);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.ScrollBars = ScrollBars.Vertical;
+            textBox4.Size = new Size(303, 306);
+            textBox4.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1066, 450);
+            Controls.Add(textBox4);
+            Controls.Add(pictureBox1);
+            Controls.Add(label9);
+            Controls.Add(textBox3);
+            Controls.Add(label8);
+            Controls.Add(textBox2);
+            Controls.Add(btn);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(comboBox6);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox5);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private ComboBox comboBox4;
+        private ComboBox comboBox5;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox6;
+        private Label label7;
+        private Button button1;
+        private Button btn;
+        private TextBox textBox2;
+        private Label label8;
+        private TextBox textBox3;
+        private Label label9;
+        private PictureBox pictureBox1;
+        private TextBox textBox4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,23 @@
+using MySql.Data.MySqlClient;
+
+namespace TreesZaimov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string connstr = 
+            //MySqlConnection
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
